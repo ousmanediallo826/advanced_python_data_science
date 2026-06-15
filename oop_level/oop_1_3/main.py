9af70f4b5fc9aa429b6ad7db61ed912897379c04
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-15 09:11:47 -0400

    2. Class vs. Instance Attributes

--- oop_level/oop_1_3/main.py
+++ oop_level/oop_1_3/main.py
@@ -21,3 +21,35 @@ x.say_hi()
 y = Robot()
 y.set_name(x.get_name())
 print(y.get_name())
+
+
+
+# =============================2. Class vs. Instance Attributes=================================
+class Robot:
+    __counter = 0
+    def __init__(self):
+        type(self).__counter += 1
+
+    @staticmethod
+    def RobotInstances():
+        return Robot.__counter
+    @classmethod
+    def RoneInstances(cls):
+        return cls, Robot.__counter
+
+
+if __name__ == "__main__":
+    print(Robot.RobotInstances())
+    x = Robot()
+    print(x.RobotInstances())
+    y = Robot()
+    print(x.RobotInstances())
+    print(Robot.RobotInstances())
+
+    print(Robot.RoneInstances())
+    print(Robot.RobotInstances())
+    z = Robot()
+    print(z.RoneInstances())
+    f = Robot()
+    print(z.RoneInstances())
+    print(Robot.RoneInstances())
