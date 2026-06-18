d1df8b407ec4b3efedd2e555e97999431ac5d44f
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-17 21:28:13 -0400

    5. Dataclasses In Python

--- oop_level/oop_4_6/main.py
+++ oop_level/oop_4_6/main.py
@@ -15,3 +15,29 @@ class ImmutableRobot:
 robot = ImmutableRobot(name="RoboX", brandname="TechBot")
 print(robot.getName())
 print(robot.getBrandName())
+
+
+
+#==========================5. Dataclasses In Python+++===============================
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ImmutableRobot:
+    name: str
+    brandname: str
+
+
+robot1 = ImmutableRobot("Marvin", "NanoGuardian XR-2000")
+robot2 = ImmutableRobot("R2D2", "QuantumTech Sentinel-7")
+robot3 = ImmutableRobot("Marva", "MachinaMaster MM-42")
+
+robots = {robot1, robot2, robot3}
+
+print("The robots in the set robots:")
+for robo in robots:
+    print(robo)
+activity = {robot1: 'activated', robot2: 'activated', robot3: 'deactivated'}
+print("\nAll the activated robots:")
+for robo, mode in activity.items():
+    if mode == 'activated':
+        print(f"{robo} is activated")
