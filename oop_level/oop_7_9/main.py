6d54c02a17c331412a1ddb3078676f38aab6a9ba
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-18 11:38:44 -0400

    8. Dynamic Data Transformation

--- oop_level/oop_7_9/main.py
+++ oop_level/oop_7_9/main.py
@@ -33,3 +33,47 @@ if __name__ == "__main__":
     z = Length(4.5, "yd") + Length(1)
     print(repr(z))
     print(z)
+
+
+
+#===========================8. Dynamic Data Transformation===========================
+
+class Product:
+
+    conversion_rates = {'USD': 1, 'EUR': 0.92, 'CHF': 0.90, 'GBP': 0.79}
+
+    def __init__(self, name, price, shipping_cost, currency='USD'):
+        self.name = name
+        self._price = price
+        self._shipping_cost = shipping_cost
+        self.currency = currency
+        self._used_currency = currency
+
+
+
+    def set_currency(self, new_currency, adapt_data=False):
+        if self.currency != new_currency:
+            self.currency = new_currency
+        if adapt_data:
+            self._price = self.price
+            self._shipping_cost = self.shipping_cost
+            self._used_currency = new_currency
+
+    @property
+    def price(self):
+        return self._convert_currency(self._price)
+
+    @property
+    def shipping_cost(self):
+        return self._convert_currency(self._shipping_cost)
+
+    def _convert_currency(self, amount):
+        factor = Product.conversion_rates[self.currency] / Product.conversion_rates[self._used_currency]
+        return round(amount * factor, 2)
+
+    def __str__(self):
+        return f"Product: {self.name}, Price: {self.price} {self.currency}, Shipping Cost: {self.shipping_cost} {self.currency}"
+
+    def show_saved_data(self):
+        outstr = f"Saved Data: {self.name=}, {self.currency=}, {self._used_currency=} {self.price=}, {self.shipping_cost=}"
+        print(outstr)
