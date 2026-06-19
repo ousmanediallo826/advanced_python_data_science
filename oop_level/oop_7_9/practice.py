e864c925dcd9f79b3da6f1b8fe4c141af32cae47
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-19 18:03:51 -0400

    Learning more about dynamic data

--- oop_level/oop_7_9/practice.py
+++ oop_level/oop_7_9/practice.py
@@ -84,3 +84,67 @@ def transform_data(raw_dict, schema):
 
 
 print(transform_data(RAW_DATA, SCHEMA_MAP))
+
+
+
+
+#=======Exercise 1: Magic Methods (The "Smart Playlist")================
+
+class Playlist:
+    def __init__(self, name: str, songs: list):
+        self.name = name
+        self.songs = songs
+
+    def __str__(self):
+        return f"PlayList: {self.name}, {[ {song} for i, song in enumerate(self.songs)]}"
+
+    def __add__(self, other):
+        return Playlist(self.name + other.name, self.songs + other.songs)
+
+    def __getitem__(self, position):
+        return self.songs[position]
+
+
+
+rock = Playlist("80s Rock", ["Jump", "Livin' on a Prayer"])
+pop = Playlist("Modern Pop", ["Blinding Lights", "Jump"])
+
+print(rock)
+
+mega_mix = rock + pop
+print(mega_mix)
+
+print(mega_mix[2])
+
+
+#===================Exercise 2: Dynamic Data Transformation (The "API Payload Sanitizer")===============
+
+def sanitize_payload(raw_data, blueprint):
+    transformed_data = {}
+
+    for raw_key, (new_key, transform_func) in blueprint.items():
+        if raw_key in raw_data:
+            raw_value = raw_data[raw_key]
+            transformed_data[new_key] = transform_func(raw_value)
+        else:
+            transformed_data[new_key] = None
+    return transformed_data
+
+
+
+RAW_API_RESPONSE = {
+    "user_identifier": "10492",
+    "USER_EMAIL": "  Alex@Example.com ", # Needs whitespace stripping
+    "account_balance_usd": "150.50",     # Needs to be a float
+    "unnecessary_spy_metadata": "xyz123" # Should be dropped
+}
+
+BLUEPRINT = {
+    "user_identifier": ("id", int),
+    "USER_EMAIL": ("email", lambda s: s.strip().lower()),
+    "account_balance_usd": ("balance", float),
+    "missing_profile_pic": ("avatar_url", str) # Not in payload, should become None
+}
+
+clean_data = sanitize_payload(RAW_API_RESPONSE, BLUEPRINT)
+print(clean_data)
