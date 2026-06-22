681b2bc833398c57e13266c1db7f87a673908b80
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-06-22 16:50:56 -0400

    2. Iterators and Iterables

--- python_advanced_level/advanced_1_5/main.py
+++ python_advanced_level/advanced_1_5/main.py
@@ -46,3 +46,68 @@ def reversal_string(text):
 
 print(reversal_string("hello"))
 
+
+
+#===========2. Iterators and Iterables====================
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num)
+
+
+
+class Countdown:
+    def __init__(self, start):
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= 0:
+            raise StopIteration
+
+        value = self.current
+        self.current -= 1
+        return value
+
+
+timer = Countdown(3)
+
+for num in timer:
+    print(num)
+
+
+
+#===Problem 1: Manual Extraction===
+text = "AI"
+
+iterator = iter(text)
+
+while True:
+    try:
+        texts = next(iterator)
+        print(texts)
+    except StopIteration:
+        break
+
+
+#===Problem 2: The Custom Multiplier Iterator===
+class PowerOfTwo:
+    def __init__(self, max_component):
+        self.max_component = max_component
+        self.exponent = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.exponent == self.max_component:
+            raise StopIteration
+        result =  2 ** self.exponent
+        self.exponent += 1
+        return result
+
+
+
+for val in PowerOfTwo(5):
+    print(val)
